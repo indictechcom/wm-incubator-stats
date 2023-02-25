955272ec7214bc9e80311cbcae06c0381f176def
Aman Kumar Singh <amankumarsingh.professional@gmail.com>
2023-02-25 11:48:25 +0530

    feat: use dash_table to display table, remove sort input

--- app.py
+++ app.py
@@ -16,12 +16,14 @@ curr_time = datetime.now()
 curr_file_path = f'{stats_path}{curr_time.strftime("%B").lower()}_{str(curr_time.year)}.tsv'
 main_df = pd.read_csv(curr_file_path, sep='\t')
 
+application.layout = dash_table.DataTable(main_df.to_dict('records'), [{"name": i, "id": i} for i in main_df.columns])
+
 wikis_list = list(main_df.project.unique())
 lang_list = ['(all)'] + list(main_df.sort_values('language code')['language code'].unique())
 wiki_selector = dcc.Dropdown(wikis_list, value=wikis_list, multi=True)
-sort_selector = dcc.Dropdown(['language code', 'Total number of edits', 'Total number of unique editors', 'Total number of pages', \
-    'Average number of edits per month (last 3 months)', 'Average number of editors per month (last 3 months)', \
-    'Total number of bytes added (last 30 days)', 'Total number of bytes removed (last 30 days)'], value='Total number of edits')
+# sort_selector = dcc.Dropdown(['language code', 'Total number of edits', 'Total number of unique editors', 'Total number of pages', \
+#     'Average number of edits per month (last 3 months)', 'Average number of editors per month (last 3 months)', \
+#     'Total number of bytes added (last 30 days)', 'Total number of bytes removed (last 30 days)'], value='Total number of edits')
 
 application.layout = dbc.Container([
     html.Br(),
@@ -31,8 +33,8 @@ application.layout = dbc.Container([
         dbc.Col([
             html.P('Project'),
             wiki_selector], md=7),
-        dbc.Col([html.P('Sort by'),
-                sort_selector], md=5)
+        # dbc.Col([html.P('Sort by'),
+        #         sort_selector], md=5)
     ]),
     html.Div(children=[dcc.Graph(id ='overview_table')], style = {'display': 'inline-block', 'width': '100%',}),
     dbc.Row([
@@ -48,14 +50,14 @@ application.layout = dbc.Container([
 @application.callback(
     Output('overview_table', 'figure'),
     Input(wiki_selector, 'value'),
-    Input(sort_selector, 'value')
+    # Input(sort_selector, 'value')
 )
-def update_overview_table(wiki_selection, sort_selection):
+def update_overview_table(wiki_selection):
     overview_df = main_df[main_df.project.isin(wiki_selection)]
-    if sort_selection == 'language code':
-        overview_df = overview_df.sort_values(sort_selection)
-    else:
-        overview_df = overview_df.sort_values(sort_selection, ascending=False)
+    # if sort_selection == 'language code':
+    #     overview_df = overview_df.sort_values(sort_selection)
+    # else:
+    #     overview_df = overview_df.sort_values(sort_selection, ascending=False)
     overview_table = go.Figure(data=[go.Table(
         header=dict(values=list(overview_df.columns), align='left', fill_color='paleturquoise', font=dict(size=14)),
         cells=dict(values=[overview_df[col] for col in overview_df.columns], align='left', height=27.5,  fill_color='mintcream', font=dict(size=14)))
@@ -64,6 +66,6 @@ def update_overview_table(wiki_selection, sort_selection):
     return overview_table
 
 if __name__ == '__main__':
-    application.run()
+    application.run_server(debug=True)
 
 app = application.server

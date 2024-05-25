71b90fa90b3cee6ee29e63d98906234f4c13ed21
Chinmay <chinmayajith30@gmail.com>
2024-05-26 00:50:45 +0530

    feat: create a flask backend server

--- cron.py
+++ cron.py
@@ -1,10 +1,2 @@
-import pandas as pd
-import datetime
-import os
-
-df = pd.read_csv('test.csv')
-sampleData = df.sample(n=100)
-dir1 = "stats"
-curr_time = datetime.datetime.now()
-newFile = os.path.join(dir1, f"{curr_time}.csv")
-sampleData.to_csv(newFile, index=False)
+from df_processor import fetch_data
+fetch_data()

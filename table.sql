39c3d0b9ad3bb05d40392b4f5583bc572a70b787
Shivanshuk28 <2contactshivanshu@gmail.com>
2024-12-22 11:15:33 +0530

    schema error fixed

--- table.sql
+++ table.sql
@@ -1,11 +1,13 @@
-CREATE TABLE incubator_dashboard (
-    project VARCHAR(50) ,                -- Project name, e.g., 'wikipedia', 'wiktionary'
-    language_code VARCHAR(10),         -- Language code, e.g., 'en', 'fr'
-    edit_count INT ,                    -- Total number of edits
-    actor_count INT ,                   -- Total number of distinct editors
-    pages_count INT ,                   -- Total number of distinct pages
-    bytes_removed_30D INT ,             -- Total bytes removed in the last 30 days
-    bytes_added_30D INT ,               -- Total bytes added in the last 30 days
-    avg_edits_3M INT ,                  -- Average edits over the last 3 months
-    avg_editors_3M INT                 -- Average editors over the last 3 months
-);
+CREATE TABLE IF NOT EXISTS incubator_dashboard (
+    project VARCHAR(50) COMMENT "specifies the name of wiki projects e.g., wikipedia, wiktionary" ,
+    language_code VARCHAR(10) COMMENT "specifies the Language code" ,         
+    edit_count INT COMMENT "the total number of edits made",                    
+    actor_count INT COMMENT "the total numbers of distinct editors ",                
+    pages_count INT COMMENT "Total number of distinct pages",
+    bytes_removed_30D INT COMMENT "Total bytes removed in the last 30 days",             
+    bytes_added_30D INT COMMENT "Total bytes added in the last 30 days",               
+    avg_edits_3M INT COMMENT "Average edits over the last 3 months",                  
+    avg_editors_3M INT COMMENT "Average editors over the last 3 months"                 
+)
+ENGINE=InnoDB
+COMMENT = 'test wikis in the Wikimedia Incubator which are most active during the last few months';
